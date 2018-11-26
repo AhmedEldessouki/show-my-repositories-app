cba0815d0643b5631d55d3292ed91a62231b77dd
AhmedEldessouki <44158955+AhmedEldessouki@users.noreply.github.com>
2018-11-26 15:18:55 +0300

    getting the app ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Home from './components/home/Home';
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Home />
       </div>
     );
   }
