b71ba1b16392fa92534285b2249d3d35667dd149
AhmedEldessouki <44158955+AhmedEldessouki@users.noreply.github.com>
2018-11-27 01:58:24 +0300

    stage 1: adding welcome, username, number of followers and following, link to the user account

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,6 +7,7 @@ export default class Home extends Component {
 		super();
 		this.state = {
 			repos: [],
+			profile: ''
 		}
 	}
 
@@ -14,32 +15,64 @@ export default class Home extends Component {
 		axios.get(`https://api.github.com/users/ahmedeldessouki/repos`).then((res) => {
 			this.setState({
 				repos: res.data
+			}, function () {
+				console.log('my repos: ', this.state.repos)
 			})
 		}).catch((err) => {
 			console.log(err);//todo we don't show the user the error in console. we remove this in the future
 		});
+		axios.get(`https://api.github.com/users/ahmedeldessouki`)
+			.then((res) => {
+				this.setState({
+					profile: res.data
+				}, function () {
+					console.log('this is the profile data: ', this.state.profile);
+				})
+			}).catch((err) => {
+				console.log(err);//todo we don't show the user the error in console. we remove this in the future
+			});
 	}
 
 	render() {
+		const { profile, repos } = this.state
 		return (
 			<div className="Home">
-        <header>
-          <h1>My Repos</h1>
-        </header>
+				<header>
+					<h1>Welcome {profile.name}</h1>
+				</header>
 				<main>
-          <ul className="repos-container">
-            {
-							this.state.repos.map((item, i) => (
-								<li key={i} className="item-container">
-									<a href={item.html_url}>
-										{item.name}
-									</a>
-                </li>
-							))
-						}
-          </ul>
+					<div className="user-components">
+						<div className="left-container">
+							<img src={profile.avatar_url} alt="profilePicture" />
+						</div>
+						<ul className="right-container">
+							<li>{profile.login}</li>
+							<li className="follow-container">
+								<h5>Followers</h5>
+								{profile.followers}
+							</li>
+							<li className="follow-container">
+								<h5>Following</h5>
+								{profile.following}
+							</li>
+							<li><a href={profile.url}>
+								Profile Link
+								</a>
+							</li>
+						</ul>
+					</div>
+					<h1>Repos</h1>
+					<ul className="repos-container">
+						{repos.map((item, i) => (
+							<li key={i} className="item-container">
+								<a href={item.html_url}>
+									{item.name}
+								</a>
+							</li>
+						))}
+					</ul>
 				</main>
-      </div>
+			</div>
 		)
 	}
 }
