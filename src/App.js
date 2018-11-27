843f82072b67e3eb080a49427686535e7a7e6930
AhmedEldessouki <44158955+AhmedEldessouki@users.noreply.github.com>
2018-11-27 14:19:34 +0300

    added navigation system with css, route, router, and switch for routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from './components/home/Home';
 import Weather from './components/weather/Weather';
+import NotFound from './components/notFound/NotFound';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
 
 
 
@@ -10,8 +13,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* <Home /> */}
-        <Weather />
+        <Navigation />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          {/* both /roster and /roster/:number begin with /roster */}
+          <Route path='/weather' component={Weather} />
+          <Route path="/404" component={NotFound} />
+          <Redirect from="*" to="/404" />
+        </Switch>
       </div>
     );
   }
