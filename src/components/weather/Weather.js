26af6cad4c48e3c13e95c222d3ac3c78d8176cf5
hesham <heshamelmasry1992@gmail.com>
2018-11-28 12:58:43 +0300

    bad import fixes, wrong way of iteration
    https://thinkster.io/tutorials/iterating-and-rendering-loops-in-react >> refer to learn how to itirate on arrays.
    
    https://stackoverflow.com/questions/45658200/how-do-i-use-include-third-party-libraries-in-react >> how to import in react
    
    -- stop using undefined
    
    please read this :+1:
    https://developmentarc.gitbooks.io/react-indepth/content/life_cycle/birth/initialization_and_construction.html
    https://stackoverflow.com/questions/51960138/reactjs-how-to-initialize-array-object-in-state
     please read the todos

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -2,18 +2,19 @@ import React, { Component } from "react"
 import "./Weather.css"
 import axios from "axios"
 import { geolocated } from "react-geolocated";
+import kelvinToCelsius from "kelvin-to-celsius"; //todo this is how you import libraries
 
 class Weather extends Component {
   constructor() {
     super();
     this.state = {
-      lon: '',
-      lat: '',
-      weatherData: undefined,
-      city: undefined,
-      country: undefined,
+      lon: null,
+      lat: null,
+      weatherData: null,
+      city: null,
+      country: null,
       isLoading: true
-    }
+    };
     this.fetchData = this.fetchData.bind(this);
   }
 
@@ -23,7 +24,8 @@ class Weather extends Component {
         lat: nextProps.coords.latitude,
         lon: nextProps.coords.longitude,
       }, function () {
-        this.fetchData(this.state.lat, this.state.lon)
+				let self = this; // save object reference
+				self.fetchData(this.state.lat, this.state.lon)
       })
     }
   }
@@ -33,39 +35,48 @@ class Weather extends Component {
       .then((res) => {
         this.setState({
           weatherData: res.data,
-          city: res.data.city.name,
-          country: res.data.city.country,
+          city: res.data.city.name,// todo not ver important to do like that because you can simple use the "weatherData" ONLY!
+          country: res.data.city.country,// todo not ver important to do like that because you can simple use the "weatherData" ONLY!
           isLoading: false,
-        })
+        },function () {
+          console.log(this.state.weatherData);//todo please remove this shit after you test. i added it to see the data
+				})
       })
-      .catch((error) => this.setState({ error, isLoading: false }));
+      .catch((error) => this.setState({ error, isLoading: false }));//todo please handle errors correctly
   }
 
-  createArr = () => {
-    const { city, country, weatherData } = this.state
-    const kelvinToCelsius = require('kelvin-to-celsius');
-    let arrTemp = []
-    for (let i = 0; i < weatherData.list.length; i += 8) {
-      arrTemp.push(
-        <div className="display-conditions">
-          <p className="display-conditions-item">Location : {city}, {country}</p>
-          <p className="display-conditions-item">Temperature: {kelvinToCelsius(weatherData.list[i].main.temp)} C</p>
-          <p className="weather-condition-container display-conditions-item">
-            <img className="img-fluid" alt="icon" src={`http://openweathermap.org/img/w/${weatherData.list[i].weather[0].icon}.png`} />
-            Weather Condition: <em>{weatherData.list[i].weather[0].description}</em>
-          </p>
-          <p className="display-conditions-item">Humidity: {weatherData.list[i].main.humidity}</p>
-          <p className="display-conditions-item">Wind Speed: {weatherData.list[i].wind.speed}</p>
-        </div>
-      )
-    }
-    return arrTemp
-  }
+  // weatherData = () => {
+  //   const { city, country, weatherData } = this.state;
+  //   // let arrTemp = [];
+  //   // for (let i = 0; i < weatherData.list.length; i += 8) {
+  //   //   arrTemp.push(
+  //   //     <div className="display-conditions">
+  //   //       <p className="display-conditions-item">Location : {city}, {country}</p>
+  //   //       <p className="display-conditions-item">Temperature: {kelvinToCelsius(weatherData.list[i].main.temp)} C</p>
+  //   //       <p className="weather-condition-container display-conditions-item">
+  //   //         <img className="img-fluid" alt="icon" src={`http://openweathermap.org/img/w/${weatherData.list[i].weather[0].icon}.png`} />
+  //   //         Weather Condition: <em>{weatherData.list[i].weather[0].description}</em>
+  //   //       </p>
+  //   //       <p className="display-conditions-item">Humidity: {weatherData.list[i].main.humidity}</p>
+  //   //       <p className="display-conditions-item">Wind Speed: {weatherData.list[i].wind.speed}</p>
+  //   //     </div>
+  //   //   )
+  //   // }
+  //   return weatherData
+  // }; //todo please remove this shit and make it how it shown below
   render() {
+		const { city, country, weatherData } = this.state;
     return (
       <div className="Weather">
         {this.state.isLoading ? <p>loading data</p> :
-          this.createArr()
+          <div className="display-conditions">
+            <p className="display-conditions-item">Location : {city}, {country}</p>
+             <ul>
+                {weatherData.list.map(function(list, index){
+									return <li className="display-conditions-item" key={ index }>Temperature: {kelvinToCelsius(list.main.temp)} C</li>;
+								})}
+            </ul>
+          </div>
         }
       </div>
     )
